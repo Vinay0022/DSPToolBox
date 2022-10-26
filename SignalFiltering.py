ef262b7ebd9c68e97a1d03a166d5f8d6af518324
Vinay Bait <vinu031995@gmail.com>
2022-10-26 18:11:35 +0530

    Operations2 file added

---
+++ SignalFiltering.py
@@ -0,0 +1,22 @@
+from scipy.signal import butter
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+t_duration = np.linspace(0,0.5,2000,False)
+sign = np.sin(2*np.pi*20*t_duration) + np.sin(2*np.pi*40*t_duration)
+fig, (ax1) = plt.subplots(1, 1, sharex=True)
+ax1.plot(t_duration, sign)
+ax1.set_title('20 and 40 Hz Sinusoid')
+ax1.axis([0, 0.5, -2, 2.5])
+plt.show()
+
+from scipy import signal
+sos = butter(15, 20, 'lp', fs=2000, output='sos')
+filtd = signal.sosfilt(sos, sign)
+fig, (ax2) = plt.subplots(1, 1, sharex=True)
+ax2.plot(t_duration, filtd)
+ax2.set_title('After applying 20 Hz high-pass filter')
+ax2.axis([0, 0.5, -2, 2.5])
+ax2.set_xlabel('Time (seconds)')
+plt.tight_layout()
+plt.show()
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+# from crypt import methods
 import json
 from types import DynamicClassAttribute
 from app import app
@@ -8,6 +9,58 @@ from flask import render_template,redirect,request
 @app.route("/")
 def index():
     return render_template("index2.html") 
+
+@app.route("/Practise2.html", methods=['GET','POST'])
+def Practise2():
+    list = []
+    list2 = []
+    list3=[]
+    if request.method == "POST":
+       num1 = request.form.get('num1')
+       fnum1 =float(num1) 
+       list.append(fnum1)
+       num2 = request.form.get('num2')
+       num3 = request.form.get('num3')
+       num4 = request.form.get('num4')
+       num5 = request.form.get('num5')
+       num6 = request.form.get('num6')
+       num7 = request.form.get('num7')
+       num8 = request.form.get('num8')
+       
+       fnum1 =float(num1) 
+       fnum2 =float(num2) 
+       fnum3 =float(num3) 
+       fnum4 =float(num4) 
+       fnum5 =float(num5) 
+       fnum6 =float(num6) 
+       fnum7 =float(num7) 
+       fnum8 =float(num8) 
+
+       list.append(fnum1)
+       list.append(fnum2)
+       list.append(fnum3)
+       list.append(fnum4)
+
+       list2.append(fnum5)
+       list2.append(fnum6)
+       list2.append(fnum7)
+       list2.append(fnum8)
+       print(list)
+       print(list2)
+
+       arr1 = np.array(list,dtype=float)
+       arr2 = np.array(list2,dtype=float)
+       list4 =np.convolve(arr1,arr2,mode='full')
+       json_str = json.dumps({'nums': list4.tolist()})
+
+       print(json_str)  # 👉️ {"nums": [1, 2, 3, 4]}
+       print(type(json_str))  # 👉️ <class 'str'>
+       print("Result\n",np.convolve(arr1,arr2,mode='full'))
+    #    list3 =np.convolve(arr1,arr2,mode='valid') 
+       return json_str 
+       print(list)
+
+    return render_template("Practise2.html")
 
 @app.route("/index2.html")
 def index2():
@@ -75,3 +128,5 @@ def Pracitse():
     #    list3 =np.convolve(arr1,arr2,mode='valid') 
        return json_str 
  return render_template("Practise.html")
+
+

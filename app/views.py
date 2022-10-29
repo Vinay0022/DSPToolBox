a25c3ad397ad58f6d17202535964be8ff1c02927
Vinay Bait <vinu031995@gmail.com>
2022-10-30 00:10:46 +0530

    Matrix Operations added

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from copy import copy
 import json
 import os
 # `from os import sendfile
@@ -7,19 +8,90 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy 
-from flask import render_template,redirect,request
+from flask import render_template,redirect,request,jsonify,abort
+from main import*
 
+#Filtering code
 picFolder = os.path.join('pics')
 app.config['UPLOAD_FOLDER']=picFolder
 @app.route("/")
 def index():
     return render_template("index2.html")
 
+#Matrix
+@app.route('/MatrixButton.html')
+def MatrixButton():
+    return render_template("MatrixButton.html")
 
+
+
+
+@app.route('/twoByTwo.html')
+def twoByTwo():
+	return render_template("twoByTwo.html")
+
+@app.route('/api/twoByTwo', methods=["POST", "GET"])
+def api_twoByTwo():
+	if request.method == "GET":
+		return abort(403)
+
+	try:
+		a11 = float(request.form["a11"])
+		a12 = float(request.form["a12"])
+		a21 = float(request.form["a21"])
+		a22 = float(request.form["a22"])
+		A = [None,[None,a11, a12], [None,a21, a22]]
+		matrixA = twoBytwo(copy.deepcopy(A),2)
+		returned_determinant = matrixA.determinant()
+		returned_square = str(matrixA.multiply(matrixA))
+		returned_transpose = str(matrixA.transpose())
+		if returned_determinant == 0:
+			returned_inverse = "Inverse does not exist."
+		else:
+			returned_inverse = str(matrixA.inverse())
+		return jsonify(returned_determinant, returned_transpose, returned_inverse, returned_square)
+		
+	except Exception as e:
+		return str(e)
+
+@app.route('/threeByThree.html')
+def threeByThree():
+	return render_template("threeByThree.html")
+
+@app.route('/api/threeByThree', methods=["POST", "GET"])
+def api_threeByThree():
+	if request.method == "GET":
+		return abort(403)
+
+	try:
+		a11 = float(request.form["a11"])
+		a12 = float(request.form["a12"])
+		a13 = float(request.form["a13"])
+		a21 = float(request.form["a21"])
+		a22 = float(request.form["a22"])
+		a23 = float(request.form["a23"])
+		a31 = float(request.form["a31"])
+		a32 = float(request.form["a32"])
+		a33 = float(request.form["a33"])
+		A = [None,[None,a11, a12, a13], [None,a21, a22, a23], [None,a31, a32, a33]]
+		matrixA = threeBythree(copy.deepcopy(A),3)
+		returned_determinant = matrixA.determinant()
+		returned_square = str(matrixA.multiply(matrixA))
+		returned_transpose = str(matrixA.transpose())
+		if returned_determinant == 0:
+			returned_inverse = "Inverse does not exist."
+		else:
+			returned_inverse = str(matrixA.inverse())
+		return jsonify(returned_determinant, returned_transpose, returned_inverse, returned_square)
+	except Exception as e:
+		return "Error: " + str(e)
+
+#Buttons 
 @app.route("/Button.html")
 def button():
     return render_template("Button.html") 
 
+#Polynomial multiplication
 @app.route("/Polynomial.html",methods=['GET','POST'])
 def Polynomial():
  list = []
@@ -61,6 +133,7 @@ def Polynomial():
        return json_str 
  return render_template("Polynomial.html")
 
+#Convolution 3*3
 @app.route("/Convolution3by3.html",methods=['GET','POST'])
 def Convolution3by3():
  list = []
@@ -102,59 +175,9 @@ def Convolution3by3():
        return json_str 
  return render_template("Convolution3by3.html")
 
-@app.route("/Convolution2.html")
-def Convolution2():
-     list = []
-     list2 = []
-     list3=[]
-     if request.method == "POST":
-        num1 = request.form.get('num1')
-        # fnum1 =float(num1) 
-        # list.append(fnum1)
-        num2 = request.form.get('num2')
-        num3 = request.form.get('num3')
-        num4 = request.form.get('num4')
-        num5 = request.form.get('num5')
-        num6 = request.form.get('num6')
-        num7 = request.form.get('num7')
-        num8 = request.form.get('num8')
-        
-        fnum1 =float(num1) 
-        fnum2 =float(num2) 
-        fnum3 =float(num3) 
-        fnum4 =float(num4) 
-        fnum5 =float(num5) 
-        fnum6 =float(num6) 
-        fnum7 =float(num7) 
-        fnum8 =float(num8) 
-
-        list.append(fnum1)
-        list.append(fnum2)
-        list.append(fnum3)
-        list.append(fnum4)
-
-        list2.append(fnum5)
-        list2.append(fnum6)
-        list2.append(fnum7)
-        list2.append(fnum8)
-        print(list)
-        print(list2)
-
-        arr1 = np.array(list,dtype=float)
-        arr2 = np.array(list2,dtype=float)
-        list4 =np.convolve(arr1,arr2,mode='full')
-        json_str = json.dumps({'nums': list4.tolist()})
-
-        print(json_str)  # 👉️ {"nums": [1, 2, 3, 4]}
-        print(type(json_str))  # 👉️ <class 'str'>
-        print("Result\n",np.convolve(arr1,arr2,mode='full'))
-        #    list3 =np.convolve(arr1,arr2,mode='valid') 
-        return json_str 
-        print(list)
-
-     return render_template("Convolution2.html")
 
 
+#Filtering code
 @app.route("/upload.html", methods=['GET','POST'])
 def upload():
     #  if request.method =="POST":
@@ -194,76 +217,20 @@ def upload():
         pic1 = os.path.join(app.config['UPLOAD_FOLDER'],'noise2.png')
         return render_template("upload.html")
 
-@app.route("/Practise2.html", methods=['GET','POST'])
-def Practise2():
-    list = []
-    list2 = []
-    list3=[]
-    if request.method == "POST":
-       num1 = request.form.get('num1')
-       fnum1 =float(num1) 
-       list.append(fnum1)
-       num2 = request.form.get('num2')
-       num3 = request.form.get('num3')
-       num4 = request.form.get('num4')
-       num5 = request.form.get('num5')
-       num6 = request.form.get('num6')
-       num7 = request.form.get('num7')
-       num8 = request.form.get('num8')
-       
-       fnum1 =float(num1) 
-       fnum2 =float(num2) 
-       fnum3 =float(num3) 
-       fnum4 =float(num4) 
-       fnum5 =float(num5) 
-       fnum6 =float(num6) 
-       fnum7 =float(num7) 
-       fnum8 =float(num8) 
 
-       list.append(fnum1)
-       list.append(fnum2)
-       list.append(fnum3)
-       list.append(fnum4)
-
-       list2.append(fnum5)
-       list2.append(fnum6)
-       list2.append(fnum7)
-       list2.append(fnum8)
-       print(list)
-       print(list2)
-
-       arr1 = np.array(list,dtype=float)
-       arr2 = np.array(list2,dtype=float)
-       list4 =np.convolve(arr1,arr2,mode='full')
-       json_str = json.dumps({'nums': list4.tolist()})
-
-       print(json_str)  # 👉️ {"nums": [1, 2, 3, 4]}
-       print(type(json_str))  # 👉️ <class 'str'>
-       print("Result\n",np.convolve(arr1,arr2,mode='full'))
-    #    list3 =np.convolve(arr1,arr2,mode='valid') 
-       return json_str 
-       print(list)
-
-    return render_template("Practise2.html")
-
+#First page
 @app.route("/index2.html")
 def index2():
      return render_template("index2.html")
+
 
 @app.route("/Operations2.html")
 def Operations():
     return render_template("Operations2.html")
 
 
-@app.route("/Convolution.html", methods=['GET','POST'])
-def Convolution():
-    if request.method == "POST":
-        num1 = request.form.get('num1')
-        num2 = request.form.get('num2')
-        add = float(num1)*float(num2)
-        return  [add]
-    return render_template("Convolution.html")
 
+#For 4*4 Convolution
 @app.route("/Practise.html",methods=['GET','POST'])
 def Pracitse():
  list = []
---
+++ main.py
@@ -0,0 +1,168 @@
+import copy # for deepcopy function
+
+class Matrix(object):
+	'''This is the main class for all 2x2 and 3x3 matrices.
+	It contains all functions that can be used commonly for both the types of matrices that we deal with in this project.
+	It serves as the parent class for the classes: twoBytwo and threeBythree.
+	'''
+
+	def __init__(self,A,order):
+		self.A = copy.deepcopy(A)
+		self.order = order
+
+
+	def __str__(self): # overides the str function and prints the numbers in the form of a matrix (in our case, it returns HTML to facilitate printing on the webpage)
+		returned_string = "<table>"
+
+		for i in range(1,self.order+1):
+			returned_string += "<tr>"
+			for j in range(1,self.order+1):
+				returned_string += "<td>" + str(self.A[i][j]) + "</td>"
+			returned_string += "</tr>"
+
+		returned_string += '</table>'
+
+		return returned_string
+
+
+	def transpose(self): # returns the transpose of 2x2 and 3x3 matrix depending on the order of the Matrix object
+		inverted = copy.deepcopy(self.A)
+		for i in range(1,self.order+1):
+			for j in range(1,self.order+1):
+				inverted[j][i] = self.A[i][j]
+
+		return Matrix(inverted,self.order)
+
+
+	def multiply(self,other): # returns the multiplication of two square matrices of the same order
+		multi = copy.deepcopy(self.A)
+
+		for i in range(1,self.order+1):
+			for j in range(1,self.order+1):
+				for k in range(1,self.order+1):
+					multi[i][j] += self.A[i][k] * other.A[k][j]
+				multi[i][j] = multi[i][j]-self.A[i][j]
+		return Matrix(multi,self.order)
+
+
+class twoBytwo(Matrix):
+	''' This is a sub-class of the Matrix class and is specific to 2x2 matrices.
+	We use the OOP concept of inheritance to inherit the initialization, str, transpose and multiply functions from the main class.
+	This class mainly defines the determinant and inverse functions for 2x2 matrices.
+	'''
+
+	def determinant(self): # returns the determinant of the 2x2 matrix represented by the current object
+		B = self.A
+		ans = (B[1][1] * B[2][2]) - (B[2][1] * B[1][2])
+		return ans
+
+	def inverse(self): # returns the inverse of a 2x2 object if it exists, or informs the user in case the determinant does not exist
+		det = self.determinant()
+
+		if det == 0:
+			return "Inverse does not exist."
+
+		adj = copy.deepcopy(self.A)
+		adj[1][1] = round(self.A[2][2]/float(det),3)
+		adj[2][2] = round(self.A[1][1]/float(det),3)
+		adj[1][2] = round(-adj[1][2]/float(det),3)
+		adj[2][1] = round(-adj[2][1]/float(det),3)
+		return Matrix(adj,2)
+
+
+class threeBythree(Matrix):
+	''' This is a sub-class of the Matrix class and is specific to 3x3 matrices.
+	We use the OOP concept of inheritance to inherit the initialization, str, transpose and multiply functions from the main class.
+	This class mainly defines the determinant and inverse functions for 3x3 matrices.
+	'''
+
+	def determinant(self): # returns the determinant of the 3x3 matrix represented by the current object
+		B = self.A
+		ans = 0
+		for x in range(1,self.order+1):
+			z = []
+
+			for i in range(2,self.order+1):
+				for j in range(1,self.order+1):
+					if j==x:
+						pass
+					else:
+						z.append(B[i][j])
+
+			Z = [None,[None,z[0], z[1]], [None,z[2], z[3]]]
+			twoBytwoSub = twoBytwo(copy.deepcopy(Z),2)
+			subDeterminant = twoBytwoSub.determinant()
+			if x%2==0:
+				ans += -B[1][x] * subDeterminant
+			else:
+				ans += B[1][x] * subDeterminant
+		return ans
+
+
+	def inverse(self): # returns the inverse of a 3x3 object if it exists, or informs the user in case the determinant does not exist
+		B = self.A
+		cofactors = copy.deepcopy(self.A)
+		coFactorElem = 0
+
+		mainDeterminant = self.determinant()
+
+		if mainDeterminant == 0:
+			return "Inverse does not exist."
+
+		for x in range(1,4):
+			for y in range(1,4):
+				z = []
+
+				for i in range(1,4):
+					for j in range(1,4):
+						if (x==i) or (y==j):
+							pass
+						else:
+							z.append(B[i][j])
+				Z = [None,[None,z[0], z[1]], [None,z[2], z[3]]]
+				twoBytwoSub = twoBytwo(copy.deepcopy(Z),2)
+				subDeterminant = twoBytwoSub.determinant()
+				
+				if ((x+y)%2 == 0):
+					coFactorElem = subDeterminant
+				else:
+					coFactorElem = -1 * subDeterminant
+
+				cofactors[x][y] = coFactorElem
+
+		cofactorsMatrix = threeBythree(cofactors,3)
+		adjoint = cofactorsMatrix.transpose()
+
+		for i in range(1,4):
+			for j in range(1,4):
+				adjoint.A[i][j] = round(adjoint.A[i][j]/float(mainDeterminant),3)
+		return adjoint
+
+
+if __name__ == '__main__': # the following code is only for testing purposes...this module is imported into app.py for its main use in the webapp.
+	order = 2
+
+	if order == 2:
+		a11 = 1
+		a12 = 2
+		a21 = 3
+		a22 = 4
+		A = [None,[None,a11, a12], [None,a21, a22]]
+		our_matrix = twoBytwo(copy.deepcopy(A),order) 
+		x = our_matrix.inverse()
+		print(x)
+
+	if order == 3:
+		a11 = 1
+		a12 = 2
+		a13 = 3
+		a21 = 4
+		a22 = 5
+		a23 = 6
+		a31 = 7
+		a32 = 6
+		a33 = 8
+		A = [None,[None,a11, a12, a13], [None,a21, a22, a23], [None,a31, a32, a33]]
+		matrixA = threeBythree(copy.deepcopy(A),order)
+		x = matrixA.inverse()
+		print (x)
